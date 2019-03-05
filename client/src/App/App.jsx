e9d45f4709400f833b98001e4482327b479baf6e
Kevin Unkrich <kev.unkrich@gmail.com>
2019-03-04 22:45:22 -0500

    Updated app file with react router.

---
+++ client/src/App/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+          <div>
+              {alert.message &&
+                  <div className={`alert ${alert.type}`}>{alert.message}</div>
+              }
+              <Router history={history}>
+                  <div>
+                  </div>
+              </Router>
+          </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App }; 
