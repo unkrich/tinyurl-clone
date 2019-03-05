6120363ee429199ec85f523e970138c46d65b7fd
Kevin Unkrich <kev.unkrich@gmail.com>
2019-03-04 20:55:34 -0500

    Added connection to mongodb with mongoose

--- server/index.js
+++ server/index.js
@@ -6,6 +6,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to unkri.ch!"});
 });
