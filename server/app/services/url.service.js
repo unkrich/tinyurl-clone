f77b8ac088e6f51890b69c3a47d3a0e70b65535d
Kevin Unkrich <kev.unkrich@gmail.com>
2019-03-19 14:24:40 -0400

    Added http/s verification to make sure react recognizes as an external link.

--- server/app/services/url.service.js
+++ server/app/services/url.service.js
@@ -12,11 +12,20 @@ module.exports = {
     _delete
 };
 
+function addhttp(url) {
+   if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+   }
+   return url;
+}
+
 // Create and Save a new url
 async function create({ originalUrl, alias, expiresAt }, { authorization }) {
     // Validate URL
     if(!originalUrl) {
         throw "Url attribute originalUrl can not be empty";
+    } else {
+        originalUrl = addhttp(originalUrl);
     }
 
     // Validate alias if provided, or generate
